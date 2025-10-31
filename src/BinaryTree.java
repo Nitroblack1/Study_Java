ec38786865d42d4f5b44e802e43bf6b1934069cc
Nitroblack1 <nitroblue@naver.com>
2025-10-31 13:41:41 +0900

    tree study

---
+++ src/BinaryTree.java
@@ -0,0 +1,9 @@
+public class BinaryTree {
+    Node root;
+
+    public BinaryTree() { root = null; }
+
+    public void insert(int i) {}
+    public Node delete(int i) { return null; }
+    public Node find(int i) { return null; }
+}
--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int i)
+    {
+        value = i;
+        left = null;
+        right = null;
+    }
+
+    public Node getLeftChild(Node node) { return left; }
+    public Node getRightChild(Node node) { return right; }
+    public void setLeftChild(Node node) { left = node; }
+    public void setRightChild(Node node) { right = node; }
 }
